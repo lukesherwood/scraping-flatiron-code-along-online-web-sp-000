ed561d2d362ffb5750f65777ae20117a7d6463de
Luke Sherwood <luke.sherwood55@gmail.com>
2020-01-31 23:19:46 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,6 +1,10 @@
 require 'nokogiri'
 require 'open-uri'
+<<<<<<< HEAD
  
+=======
+require 'pry'
+>>>>>>> e97b5caae59caa91db54ccb05165bed7ccea844f
 require_relative './course.rb'
  
 class Scraper
@@ -32,7 +36,26 @@ class Scraper
       end
     end
   end
+<<<<<<< HEAD
  
+=======
+  
+  def get_page
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    
+    doc.css(".post").each do |post|
+      course = Course.new
+      course.title = post.css("h2").text
+      course.schedule = post.css(".date").text
+      course.description = post.css("p").text
+    end
+  end
+  
+  def get_courses
+    
+  end
+  
+>>>>>>> e97b5caae59caa91db54ccb05165bed7ccea844f
 end
  
 Scraper.new.print_courses
